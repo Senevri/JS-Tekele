01ec7fd398dfd380be3f01851f379c569e039a01
Esa Karjalainen <esa.karjalainen@norfello.com>
2012-01-25 18:24:58 +0200

    minimal Python3 server for ajax testing

---
+++ server_example.py
@@ -0,0 +1,66 @@
+"""
+    Serves http.
+"""
+
+import socketserver
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    path = '/home/esa/tekele/JS-Tekele'
+    buffer = []
+
+    header = "HTTP/1.1 200 OK\r\nConnection: close\r\n \
+            Content-Type: text/html; charset=UTF-8"
+    def handle(self):
+        import struct
+        import base64
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        self.buffer.append(self.data)
+        #print ("{} wrote:".format(self.client_address[2]))
+        print(self.client_address[0], self.data)
+        strbytes = str(self.data)
+        lines = strbytes.lstrip('b').strip("'").split('\\r\\n')
+        reply = ""
+        try: 
+            for cmd, param in [l.split(' ', 1) for l in lines]:
+                if cmd.upper() == 'GET':
+                    file_req = param.split(' ', 1)[0]
+                    if file_req == '/':
+                        file_req = '/index.html'
+                    print(file_req)
+                    f = open(''.join([self.path, file_req]), 'r')
+                    try:
+                        reply = f.read()
+                    except UnicodeDecodeError as e:
+                        f.close()
+                        f = open(''.join([self.path, file_req]), 'rb')
+                        reply=f.read()
+                        #reply = str(base64.b64encode(reply))
+                        self.header = '\r\n'.join([self.header, 
+                            'Content-Encoding: binary'])
+
+                    finally:
+                        f.close()
+        except ValueError:
+            print (strbytes)
+
+        c_len = "Content-Length: {}\r\n".format(len(reply))
+        header = "HTTP/1.1 404 FAILED\r\n\r\n"
+        if len(reply)>0:
+            if type(reply) == str:
+                reply = '\r\n'.join([self.header, c_len, reply])
+                self.request.send(reply.encode())
+            else:
+                self.request.send(reply)
+
+        
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
