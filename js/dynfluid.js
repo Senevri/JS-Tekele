c796d846a32acc9f4cd5946cde41b539736136fd
Esa Karjalainen <esa.karjalainen@haaveinc.com>
2012-05-13 02:57:31 +0300

    tweaking dynfluid; not working, though

--- js/dynfluid.js
+++ js/dynfluid.js
@@ -1,13 +1,22 @@
+$(document).ready(
 (function (){
-
+$('#main').css('background-color', 'gray');
 function sleep(ms)
-	{
+    {
+        if (ms <= 0 ) {
+            return;
+        }
 		var dt = new Date();
 		dt.setTime(dt.getTime() + ms);
-		while (new Date().getTime() < dt.getTime());
+		if (new Date() < dt) {
+            setTimeout(function () {
+                sleep(ms-1);
+            }, (dt.getTime() - new Date().getTime()));
+        }
 	}
 
-var input = window.input;
+//var input = window.input.getInstance();
+var input = new Input();
 var widgets = [];
 
 //dynfluid.js
@@ -18,37 +27,31 @@ widgets.push(new Widget('init'));
 foo = new Widget('foo');
 kangas = new Widget('kangas');
 //foo.add("<p>hello world</p>");
-kangas.add("<canvas id='dk' width=600 height=300></canvas>");
-foo.add("<a href='' onclick='step()'>klik</a>")
+kangas.add("<canvas id='dk' width=640 height=300></canvas>");
+foo.add("<a href='' onclick='step()'>klik</a>");
 //widgets.push(foo);
 widgets.push(kangas);
 //drawWidgetsTo('main');
 for (i in widgets){
-	widgets[i].drawTo('main')
+	widgets[i].drawTo('main');
 }
 var canvas = document.getElementById('dk');
+canvas.style.backgroundColor = 'black';
 canvas.style.marginLeft = 'auto';
 canvas.style.marginRight = 'auto';
 if (canvas.getContext){
+    var height = 40, width = 60;
 	/* do stuff here*/
 	//----
 	var x = 0;
 	var n = 0;
 	var surface = [];
-	//var momentum = [];
-	
 	var i=0;
-	while(i<100){
-		surface.push(Math.floor(Math.random()*255));
+
+	while(i<height*width){
+		surface.push(Math.floor(Math.random()));
 		i++;
 	}
-	//surface[0] = 0;
-	//surface[25] = 0;
-	//surface[50] = 255
-	//surface[45] = 255
-	//surface[60] = 255
-	//surface[88] = 255
-	//surface[99] = 255
 	var momentum = surface.slice(0);
 	while( i < surface.length){
 		momentum[i]=0;
@@ -59,118 +62,169 @@ if (canvas.getContext){
 	
 	var j = 0;
 	//doStuff(ctx);
-	//stamGrid(ctx); 
+	stamGrid(ctx); 
 	time = 0;
-	stamGrid_wrap(ctx);	
+	//stamGrid_wrap(ctx);	
 	//----
 	
 }
 
 var time;
 function stamGrid_wrap(ctx){
+	var GtoL = function(x, y){
+		return (y*width+x);
+	};
 	time += 1;
-	if (time == 20){
+	if (time == 200){
 		time = 0;
-		surface[44]=255;
+        surface[GtoL(height/2, width/2)] = 1;
+        surface[GtoL(10, 10)] = 0;
+        surface[GtoL(10, 12)] = 1;
 	}
-	stamGrid(ctx);
-	setTimeout(function() { stamGrid_wrap(ctx) }, 30);
+	//stamGrid(ctx);
+	//setTimeout(function() { stamGrid_wrap(ctx); }, 5);
 }
 
 function stamGrid(ctx){
 	//käytetään surfacea 10x10 gridinä 
 	var GtoL = function(x, y){
-		return (y*10+x);
-	}
+		return (y*width+x);
+	};
 	var LtoG = function(i){
-		var y = Math.floor(i/10);
-		var x = i-(10*y);
-		return {'x':x, 'y':y}
+		var y = Math.floor(i/width);
+		var x = i-(width*y);
+		return {'x':x, 'y':y};
 		//return [x,y];
-	}
+	};
+
 	this.GtoL = GtoL;
 	this.LtoG = LtoG;
 	//diffuse
 	
 	i = 0;
-	
-	while( i < surface.length-1){
-	 /* for each location in the grid 
-	  * if density is higher than neighbours, exchange some mass with
-	  * the neighbour.
-	  */
-		//left, right, up, down
-		var cur = surface[i];
-		var curm = cur+momentum[i];
-		var coords = LtoG(i);
-		var lost_mass = 0;
-		if (coords.x<9 && curm>surface[i+1]) {
-			lost_mass++;
-			surface[i+1]++;
-			momentum[i+1]=(cur-surface[i+1]);
-		}
-		if (coords.x>0 && curm>surface[i-1]) {
-			lost_mass++;
-			surface[i-1]++;
-			momentum[i-1] +=(cur-surface[i-1]);
-		}
-		//var cur = surface[i];
-		var loc = GtoL(coords.x, coords.y-1);
-		var curm = cur+momentum[loc];
-		if (coords.y>0 && curm>surface[loc]){
-			lost_mass++;
-			surface[loc]++;
-			momentum[loc] +=(cur-surface[i-10]);
-		}	
-		loc = GtoL(coords.x, coords.y+1);
-		if(coords.y<9 && curm>surface[loc]){
-			lost_mass++;
-			surface[loc]++;
-			momentum[loc] +=(cur-surface[i+10]);
-		}
-		surface[i] = surface[i]-(lost_mass);
-		i++;			
-		//debug.write("foo");
-	}
-	
-	i = 0;
-	while( i < surface.length){
-	 /* for each location in the grid
-	  * if there's momentum
-	  */
-		//left, right, up, down
-		var cur = surface[i];
-		var coords = LtoG(i);
-		var lost_mass = 0;
-		if(momentum[i]>0) {
-			//surface[i] = surface[i] - 4;
-			momentum[i]=momentum[i]-4;
-			if (coords.x<9) {
-				surface[i+1] = surface[i+1] + 1;
-				} else {
-				momentum[i]++	
-			}
-			if (coords.x>0) {
-				surface[i-1] = surface[i-1] + 1;	
-				} else {
-				momentum[i]++	
-			}
-			//var cur = surface[i];
-			var loc = GtoL(coords.x, coords.y-1);
-			if (coords.y>0){
-				surface[i-10] = surface[i-10] + 1;
-				}	
-			loc = GtoL(coords.x, coords.y+1);
-			if(coords.y<9){
-				surface[i+10] = surface[i+10] + 1;
-				}
+    (function () {	
+        while( i < surface.length-1){
+            /* for each location in the grid 
+             * if density is higher than neighbours, exchange some mass with
+             * the neighbour.
+             */
+            var viscosity = 1;
+            var coords = LtoG(i);
+            var delta = 0.0;
+            var src, dest;
+            var sum_delta = 0;
+            if (coords.x < width-1) {
+                src = surface[i];
+                dest = surface[i+1];
+                if (dest < src) {
+                    delta = viscosity * (src-dest)/2;
+                    surface[i+1] += delta;
+                    sum_delta += delta;
+                }
+            }
+            if (coords.y < height-1) {
+                src = surface[i];
+                dest = surface[i+width];
+                if (dest < src) {
+                    delta = viscosity * (src-dest)/2;
+                    surface[i+width] += delta;
+                    sum_delta += delta;
+                }
+            }
+            if (coords.x > 0) {
+                src = surface[i];
+                dest = surface[i-1];
+                if (surface[i-1] < surface[i]) {
+                    delta = viscosity * (src-dest)/2;
+                    surface[i-1] += delta;
+                    sum_delta += delta;
+                }
+            }
+            if (coords.y > 0) {
+                src = surface[i];
+                dest = surface[i-width];
+                if (surface[i-width] < surface[i]) {
+                    delta = viscosity * (src-dest)/2;
+                    surface[i-width] += delta;
+                    sum_delta += delta;
+                }
+            }
+            surface[i] -= sum_delta;
+            //console.log(sum_delta);
+            i += 1;
+            //left, right, up, down
+/*            var cur = surface[i];
+            var curm = cur+momentum[i];
+            var coords = LtoG(i);
+            var lost_mass = 0;
+            if (coords.x<width-1 && curm>surface[i+1]) {
+                lost_mass++;
+                surface[i+1]++;
+                momentum[i+1]=(cur-surface[i+1]);
+            }
+            if (coords.x>0 && curm>surface[i-1]) {
+                lost_mass++;
+                surface[i-1]++;
+                momentum[i-1] +=(cur-surface[i-1]);
+            }
+            //var cur = surface[i];
+            var loc = GtoL(coords.x, coords.y-1);
+            curm = cur+momentum[loc];
+            if (coords.y>0 && curm>surface[loc]){
+                lost_mass++;
+                surface[loc]++;
+                momentum[loc] +=(cur-surface[i-width]);
+            }	
+            loc = GtoL(coords.x, coords.y+1);
+            if(coords.y<height-1 && curm>surface[loc]){
+                lost_mass++;
+                surface[loc]++;
+                momentum[loc] +=(cur-surface[i+width]);
+            }
+            surface[i] = surface[i]-(lost_mass);
+            i++;			*/
+        }
+    }());
 
-		}
-		
-		i++;			
-		//debug.write("foo");
-	}
-	
+    (function() {
+        var i = 0;
+        while( i < surface.length){
+            /* for each location in the grid
+             * if there's momentum
+             */
+            //left, right, up, down
+            var cur = surface[i];
+            var coords = LtoG(i);
+            var lost_mass = 0;
+            if(momentum[i]>0) {
+                //surface[i] = surface[i] - 4;
+                momentum[i]=momentum[i]-4;
+                if (coords.x<9) {
+                    surface[i+1] = surface[i+1] + 1;
+                } else {
+                    momentum[i] += 1;
+                }
+                if (coords.x>0) {
+                    surface[i-1] = surface[i-1] + 1;	
+                } else {
+                    momentum[i] += 1;	
+                }
+                //var cur = surface[i];
+                var loc = GtoL(coords.x, coords.y-1);
+                if (coords.y>0){
+                    surface[i-width] = surface[i-width] + 1;
+                }	
+                loc = GtoL(coords.x, coords.y+1);
+                if(coords.y<9){
+                    surface[i+width] = surface[i+width] + 1;
+                }
+
+            }
+
+            i++;			
+            //debug.write("foo");
+        }
+    });	
 	i = 0;
 	
 	
@@ -179,21 +233,21 @@ function stamGrid(ctx){
 		
 		//draw
 	ctx.beginPath();
-	ctx.moveTo(0,100);
+	ctx.moveTo(12,60);
 	var x=0; 
 	var y=0;
 	while( i < surface.length){
 		//ctx.drawColor('green');
 		//ctx.Color(surface[i]);
-		ctx.fillStyle = 'rgb(0,100,'+ surface[i] +')';
-		ctx.fillRect(x*10,y*10,10,10);
+		ctx.fillStyle = 'rgb(0,100,'+ Math.floor(surface[i]*255) +')';
+		ctx.fillRect(x*5,y*5,5,5);
 		//ctx.lineTo(i*5, surface[i]);
-		x++;
-		if (x==10) {
+		x += 1;
+		if (x==width) {
 			x = 0;
-			y++;
+			y += 1;
 		}
-		i++;
+		i += 1;
 	}	
 	ctx.fill();
 	//var t = setTimeout("stamGrid(ctx)", 200);
@@ -266,4 +320,4 @@ function doStuff(ctx){
 	var t = setTimeout("doStuff(ctx)", 200);
 }
 */
-})();
+}()) );
