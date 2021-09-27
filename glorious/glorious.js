b48bcb34c3e171866df1585c509116463d41ab27
Esa Karjalainen <esa.karjalainen@botlabs.fi>
2021-09-27 14:03:10 +0300

    Glorious: Add entities, better keyhandler

--- glorious/glorious.js
+++ glorious/glorious.js
@@ -2,11 +2,11 @@
 "use strict";
 
 	window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       || 
-              window.webkitRequestAnimationFrame || 
-              window.mozRequestAnimationFrame    || 
-              window.oRequestAnimationFrame      || 
-              window.msRequestAnimationFrame     || 
+      return  window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame    ||
+              window.oRequestAnimationFrame      ||
+              window.msRequestAnimationFrame     ||
               function( callback ){
                 window.setTimeout(callback, 1000 / 60);
               };
@@ -15,12 +15,12 @@
 /* SETUP */
 	var foo = 0;
 	var dom = {};
-	
+
 	dom.text = document.createTextNode("Helloworkd");
-	dom.canvas = document.createElement("canvas");	
+	dom.canvas = document.createElement("canvas");
 	var p = document.createElement("P")
 		.appendChild(dom.text)
-	dom.main = document.getElementById("mainWindow");	  
+	dom.main = document.getElementById("mainWindow");
 	dom.main.appendChild(p);
 	dom.main.appendChild(dom.canvas);
 	dom.canvas.style.marginTop="8px";
@@ -32,7 +32,7 @@
     dom.main.appendChild(btn);
 	console.log(dom.canvas);
 	/* relevant */
-	var ctx = dom.canvas.getContext("2d");  
+	var ctx = dom.canvas.getContext("2d");
 	ctx.fillStyle="#000000";
 	ctx.imageSmoothingEnabled = true;
 
@@ -63,12 +63,12 @@
         var fanta = resources.sfx["fanta_in_space.mp3"];
         if (fanta.paused) {
             fanta.play();
-        } else { 
+        } else {
             fanta.pause()
         };
         console.log(fanta);
     };
-	
+
 	console.log(resources);
 	var motion = { x:0, y:0 };
 	for (var key in resources.gfx) {
@@ -81,47 +81,77 @@
                 switch (self.name) {
                     default:
                 }
-                        ctx.drawImage(self, self.clientLeft, self.clientTop, 128 , 128);	  				
+                ctx.drawImage(self, self.clientLeft, self.clientTop, 128 , 128);
 			}
 			//x = ((x+128)>dom.canvas.width) ? 0: x + 128;
 		}
 	}
 
     document.addEventListener('keydown', function(event) {
-        switch (event.key) {
-            case "w":
-                motion.y -= 100;
-                break;
-            case "a":
-                motion.x -= 100; 
-                break;
-            case "s":
-                motion.y += 100; 
-                break;
-            case "d":
-                motion.x += 100; 
-                break;
-            default:
-                console.log(event.keyCode, event.key);
-            }
+		let keyhandlers = {
+			"w": ()=>motion.y -= 100,
+			"a": ()=>motion.x -= 100,
+			"s": ()=>motion.y += 100,
+			"d": ()=>motion.x += 100,
+		}
+
+		if (Object.keys(keyhandlers).includes(event.key)) {
+			//console.log(event, keyhandlers[event.key])
+			keyhandlers[event.key]()
+		}
+		else {
+
+			console.log("no handler for", event.keyCode, event.key);
+		}
     });
 
     var fps = 60
-		
+
+	class Entity{
+		name="Unnamed"
+		gfx=null
+		position={x:0,y:0}
+		constructor(name, gfx, position){
+			this.name = name
+			this.gfx = gfx
+			this.position = position ? position : this // y dis works?
+		}
+	}
+
+	var entities = [
+		new Entity("Player", resources.gfx["test.png"]),
+		new Entity("not_player", resources.gfx["test.png"], {x:100, y:100})
+	]
+
 	var heartbeat = window.setInterval(function (){
 			/*start heartbeat*/
-			dom.text.nodeValue= "Hellotime, seconds ".concat(foo*fps/1000);		
-            var player = resources.gfx["test.png"];
-            ctx.clearRect(0, 0, dom.canvas.width, dom.canvas.height);
-            ctx.drawImage(player, player.clientLeft + motion.x, player.clientTop + motion.y, 100, 100);
+			dom.text.nodeValue= "Hellotime, seconds ".concat(foo*fps/1000);
 
-			/*end heartbeat*/				
+            ctx.clearRect(0, 0, dom.canvas.width, dom.canvas.height);
+			entities.forEach(entity=>{
+				if (entity.name==="Player") {
+					entity.position.x = motion.x
+					entity.position.y = motion.y
+				} else {
+					//console.log(entity.position)
+				}
+				if (entity.gfx) {
+					ctx.drawImage(
+						entity.gfx,
+						entity.gfx.clientLeft + entity.position.x, entity.gfx.clientTop + entity.position.y,
+						100, 100);
+				}
+			})
+
+
+			/*end heartbeat*/
 			foo++;
 	}, 1000/fps);
 	//resources.sfx.test.volume = 0.5;
 	//resources.sfx.test.play();
 	//resources.sfx.test.currentTime=120;
-	
-	
-	console.log(resources);	  
+
+
+	console.log(resources);
+	console.log(entities)
 })();
